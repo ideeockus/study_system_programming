3447c925c9f24a42876036634f3ddb7511201aea
radmirkus <radmirka745@gmail.com>
2023-01-09 02:53:14 +0300

    added more debug logging & fixed infinity waiting

--- posix_producer_consumer/main.cpp
+++ posix_producer_consumer/main.cpp
@@ -44,8 +44,20 @@ void* producer_routine(void* arg) {
 
     struct thread_params* p = (struct thread_params*)arg;
 
-    pthread_mutex_lock(&my_mutex);
-    pthread_cond_wait(&my_cond_consumer_is_ready, &my_mutex);
+    int ret;
+
+    ret = pthread_mutex_lock(&my_mutex);
+    if (ret) {
+        errno = ret;
+        perror("pthread_mutex_lock");
+        return NULL;
+    }
+    ret = pthread_cond_wait(&my_cond_consumer_is_ready, &my_mutex);
+    if (ret) {
+        errno = ret;
+        perror("pthread_cond_wait");
+        return NULL;
+    }
 
     // read numbers from stdin
     std::string input;
@@ -56,19 +68,47 @@ void* producer_routine(void* arg) {
         p->my_num = num;
         p->empty_flag = false;
 
+        std::cout << "p->empty_flag " << p->empty_flag << std::endl;
         while (!p->empty_flag) {
-            pthread_cond_signal(&my_cond_full);
-            pthread_cond_wait(&my_cond_empty, &my_mutex);
+            ret = pthread_cond_signal(&my_cond_full);
+            if (ret) {
+                errno = ret;
+                perror("pthread_cond_signal");
+                return NULL;
+            }
+            std::cout << "waiting empty" << std::endl;
+            ret = pthread_cond_wait(&my_cond_empty, &my_mutex);
+            if (ret) {
+                errno = ret;
+                perror("pthread_cond_wait");
+                return NULL;
+            }
         }
+        std::cout << "oops" << std::endl;
     }
+    std::cout << "out" << std::endl;
     p->done_flag = true;
     // wakeup consumer threads, so they can return calculations
-    pthread_cond_broadcast(&my_cond_full);
-    pthread_mutex_unlock(&my_mutex);
+    ret = pthread_cond_broadcast(&my_cond_full);
+    if (ret) {
+        errno = ret;
+        perror("pthread_cond_broadcast");
+        return NULL;
+    }
+    ret = pthread_mutex_unlock(&my_mutex);
+    if (ret) {
+        errno = ret;
+        perror("pthread_mutex_unlock");
+        return NULL;
+    }
 
-    pthread_cancel(my_pthreads_ids[2]);
+    ret = pthread_cancel(my_pthreads_ids[2]);
+    if (ret) {
+        errno = ret;
+        perror("pthread_cancel");
+        return NULL;
+    }
     return NULL;
-
 
 }
 
@@ -80,20 +120,62 @@ void* consumer_routine(void* arg) {
     struct thread_params* p = (struct thread_params*)arg;
     long* local_sum = new long(0);
 
-    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &last_state);
-    pthread_cond_broadcast(&my_cond_consumer_is_ready);
+    ret = pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &last_state);
+    if (ret) {
+        errno = ret;
+        perror("pthread_setcancelstate");
+        return NULL;
+    }
+    ret = pthread_cond_broadcast(&my_cond_consumer_is_ready);
+    if (ret) {
+        errno = ret;
+        perror("pthread_cond_broadcast");
+        return NULL;
+    }
 
     while(!p->done_flag) {
-        pthread_mutex_lock(&my_mutex);
+        ret = pthread_mutex_lock(&my_mutex);
+        if (ret) {
+            errno = ret;
+            perror("pthread_mutex_lock");
+            return NULL;
+        }
         while (p->empty_flag) {
-            pthread_cond_signal(&my_cond_empty);
-            pthread_cond_wait(&my_cond_full, &my_mutex);
-
-
+            std::cout << get_tid() << " sending signal empty" << std::endl;
+            ret = pthread_cond_signal(&my_cond_empty);
+            if (ret) {
+                errno = ret;
+                perror("pthread_cond_signal");
+                return NULL;
+            }
+            std::cout << get_tid() << " waiting full, p->empty_flag " << p->empty_flag << std::endl;
+//            std::cout << my_pthreads_ids[1] << " is id of producer " << std::endl;
             if (p->done_flag) {
-                pthread_mutex_unlock(&my_mutex);
+                ret = pthread_mutex_unlock(&my_mutex);
+                if (ret) {
+                    errno = ret;
+                    perror("pthread_mutex_unlock");
+                    return NULL;
+                }
                 return  local_sum;
             }
+            ret = pthread_cond_wait(&my_cond_full, &my_mutex);
+            if (ret) {
+                errno = ret;
+                perror("pthread_cond_wait");
+                return NULL;
+            }
+
+
+//            if (p->done_flag) {
+//                ret = pthread_mutex_unlock(&my_mutex);
+//                if (ret) {
+//                    errno = ret;
+//                    perror("pthread_mutex_unlock");
+//                    return NULL;
+//                }
+//                return  local_sum;
+//            }
         }
 
         *local_sum += p->my_num;
@@ -113,12 +195,18 @@ void* consumer_interruptor_routine(void* arg) {
     pthread_mutex_lock(&my_mutex);
     pthread_cond_wait(&my_cond_consumer_is_ready, &my_mutex);
     pthread_mutex_unlock(&my_mutex);
+    int ret;
 
     while(true) {
-        pthread_t random_thread_id_to_cancel = my_pthreads_ids[rand_int(2, n_threads+2)];
+        pthread_t random_thread_id_to_cancel = my_pthreads_ids[rand_int(3, n_threads+2)];
         pthread_testcancel();
         if (random_thread_id_to_cancel == 0) continue;
-        pthread_cancel(random_thread_id_to_cancel);
+        ret = pthread_cancel(random_thread_id_to_cancel);
+        if (ret) {
+            errno = ret;
+            perror("pthread_cancel");
+            return NULL;
+        }
     }
 }
 
@@ -126,42 +214,68 @@ int run_threads() {
     // start N threads and wait until they're done
     // return aggregated sum of values
     struct thread_params p;
+    int ret;
 
     pthread_key_create(&cur_tid, NULL);
     my_pthreads_ids = new pthread_t[3 + n_threads];
 
-    int producer_created = pthread_create(&my_pthreads_ids[1], NULL, producer_routine, &p);
-    int interruptor_created = pthread_create(&my_pthreads_ids[2], NULL, consumer_interruptor_routine, NULL);
-
-    for (int i=0;i<n_threads;i++) {
-        int c = pthread_create(&my_pthreads_ids[3 + i], NULL, consumer_routine, &p);
+    ret = pthread_create(&my_pthreads_ids[1], NULL, producer_routine, &p);
+    if (ret) {
+        errno = ret;
+        perror("pthread_create");
+        return -1;
+    }
+    ret = pthread_create(&my_pthreads_ids[2], NULL, consumer_interruptor_routine, NULL);
+    if (ret) {
+        errno = ret;
+        perror("pthread_create");
+        return -1;
     }
 
-    long* consumers_results = new long[n_threads];
+    for (int i=0;i<n_threads;i++) {
+        ret = pthread_create(&my_pthreads_ids[3 + i], NULL, consumer_routine, &p);
+        if (ret) {
+            errno = ret;
+            perror("pthread_create");
+            return -1;
+        }
+    }
+
+//    long* consumers_results = new long[n_threads];
     long aggregated_sum = 0;
     for (int i=0;i<n_threads;i++) {
         // collect sums from each consumer
-        void* result_ptr = new long;
-        pthread_join(my_pthreads_ids[3 + i], &result_ptr);
-        consumers_results[i] = *(long*)result_ptr;
+        void* result_ptr = new long(0);
+        ret = pthread_join(my_pthreads_ids[3 + i], &result_ptr);
+        if (ret) {
+            errno = ret;
+            perror("pthread_join");
+            return -1;
+        }
+//        consumers_results[i] = *(long*)result_ptr;
         aggregated_sum += *(long*)result_ptr;
         delete (long*)result_ptr;
     }
 
-    for (int i=0;i<n_threads;i++) {
-        std::cout << consumers_results[i] << " ";
-    }
-    std::cout << std::endl;
-
-    delete[] consumers_results;
-
-    pthread_join(my_pthreads_ids[1], NULL);
-    pthread_join(my_pthreads_ids[2], NULL);
-//    if (ret) {
-//        errno = ret;
-//        perror("pthread_join");
-//        return -1;
+//    for (int i=0;i<n_threads;i++) {
+//        std::cout << consumers_results[i] << " ";
 //    }
+//    std::cout << std::endl;
+//
+//    delete[] consumers_results;
+
+    ret = pthread_join(my_pthreads_ids[1], NULL);
+    if (ret) {
+        errno = ret;
+        perror("pthread_join");
+        return -1;
+    }
+    ret = pthread_join(my_pthreads_ids[2], NULL);
+    if (ret) {
+        errno = ret;
+        perror("pthread_join");
+        return -1;
+    }
     delete my_pthreads_ids;
 
     return aggregated_sum;
